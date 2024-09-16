200d39d63faff176bafb9a8ba9f9b0ae1c2d92ca
1Ruslan3 <keshabyn@mail.ru>
2024-09-16 11:14:19 +0300

    13

---
+++ Thread/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Program
+{
+    public delegate void SimpleDelegate(string message);
+    public static void Main(string[] args)
+    {
+        SimpleDelegate del = PrintMessage;
+        del += PrintAnotherMessage;
+        del("Hello, Multicast!");
+        del -= PrintMessage;
+        del("Hello after unsubscribing!");
+    }
+    public static void PrintMessage(string message)
+    {
+        Console.WriteLine(message);
+    }
+    public static void PrintAnotherMessage(string message)
+    {
+        Console.WriteLine("Another: " + message);
+    }
+}
